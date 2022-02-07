bfbf107d616b1ee0eaa2301a328bfcb44153fc0b
Fagner Santiago <fagner.santiago@hotmail.com>
2022-02-07 16:19:42 -0300

    começando a achar a solução

--- CodeSignal/commonCharacterCount.js
+++ CodeSignal/commonCharacterCount.js
@@ -1,3 +1,14 @@
-function solution(s1, s2) {}
+function solution(s1, s2) {
+  let countCommonString = 0;
 
+  for (let i = 0; i < s1.length; i++) {
+    if (s2.indexOf(s1[i]) !== -1) {
+      countCommonString++;
+      console.log(s2.indexOf(s1[i]));
+    }
+    // console.log(s2.indexOf(s1[i + 1]));
+  }
+
+  return countCommonString;
+}
 console.log(solution("aabcc", "adcaa"));
