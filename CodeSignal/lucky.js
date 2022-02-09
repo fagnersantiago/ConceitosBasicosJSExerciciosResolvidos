af8052e5ffe69bdd1a4896b6fdf99fafa18056f5
Fagner Santiago <fagner.santiago@hotmail.com>
2022-02-09 18:24:26 -0300

    solução em desenvolvimento

--- CodeSignal/lucky.js
+++ CodeSignal/lucky.js
@@ -1,13 +1,22 @@
 function solution(n) {
   let string = String(n);
-  let result = string.split("", 2);
-
-  for (let i = 0; i < result.length; i++) {
-    if (parseInt(`${result[0]}`) + parseInt(`${result[i + 1]}`)) {
-      return true;
-    } else {
-      return false;
+  let result = string.split("");
+  let arr = [];
+  const sliceArray = result.reduce((acc, currentValue, currentIndex) => {
+    const index = Math.floor(currentIndex / 2);
+    if (!acc[index]) {
+      acc[index] = [];
     }
+    acc[index].push(currentValue);
+    return acc;
+  }, []);
+
+  for (let value in sliceArray) {
+    let result1 = parseInt(sliceArray[value][0]);
+    let result2 = parseInt(sliceArray[value][1]);
+    let sum = result1 + result2;
+    arr.push(sum);
   }
+  return arr;
 }
-console.log(solution(10));
+solution(121212);
