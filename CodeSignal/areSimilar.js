7421914039e2384ac7f7b375c549cfb1ad643a8b
Fagner Santiago <fagner.santiago@hotmail.com>
2022-02-22 09:51:25 -0300

    desafio concluído

--- CodeSignal/areSimilar.js
+++ CodeSignal/areSimilar.js
@@ -1,13 +1,29 @@
 function solution(a, b) {
-  let arr = [];
-  let arr2 = [];
+  let interationArrayA = [];
+  let interationArrayB = [];
 
   for (let i = 0; i < a.length; i++) {
     if (a[i] != b[i]) {
-      arr.push(b[i]);
+      interationArrayA.push(a[i]);
+      interationArrayB.push(b[i]);
     }
   }
-  console.log(arr.length > 2 ? false : true);
+
+  if (
+    interationArrayA.length === 2 &&
+    interationArrayB.reverse().toLocaleString() === arr.toLocaleString()
+  ) {
+    return true;
+  } else if (
+    interationArrayA.toLocaleString() === interationArrayB.toLocaleString()
+  ) {
+    return true;
+  }
+
+  return false;
 }
 
-solution([1, 1, 4], [1, 2, 3]);
+solution(
+  [832, 998, 148, 570, 533, 561, 894, 147, 455, 279],
+  [832, 998, 148, 570, 533, 561, 455, 147, 894, 279]
+);
